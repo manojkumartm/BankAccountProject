26f4e769ff2a6197bfdcdcbf9cc37bcca7facf5c
Manoj Kumar <manoj@gmail.com>
2017-05-09 13:17:41 +0200

    Design Completed

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,55 @@
 package com.wipro.bank.assessment;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.wipro.bank.entity.Account;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+	public static final List<Account> accounts = new ArrayList<Account>();
+	private List<Account> emp2;
+
+	public Account getAccountDetailsByID(int id) {
+		Account emp1 = null;
+		for (Account e : accounts) {
+			if (e.getAccountID() == id) {
+				emp1 = e;
+			}
+
+		}
+		return emp1;
+
+	}
+
+	public List<Account> getAccountDetailsByBalance(double amount) {
+
+		emp2 = new ArrayList<Account>();
+		for (Account e : accounts) {
+			if (e.getBalance() >= amount) {
+				emp2.add(e);
+			}
+		}
+		if (emp2.size() == 0) {
+			emp2 = null;
+		}
+		return emp2;
+
+	}
+
+	public static void main(String[] args) {
+
+		accounts.add(new Account(1, "Dhoni", 1001.11));
+		accounts.add(new Account(2, "Raina", 2001.13));
+		accounts.add(new Account(3, "Kohli", 3001.22));
+		accounts.add(new Account(4, "Rohit", 4001.55));
+		accounts.add(new Account(5, "Jaddu", 5001.77));
+		accounts.add(new Account(6, "Ash", 6001.22));
+		accounts.add(new Account(7, "Mojo", 7001.11));
+		accounts.add(new Account(7, "Sachin", 8001.99));
+
+		App temp = new App();
+		System.out.println(temp.getAccountDetailsByID(1));
+		System.out.println(temp.getAccountDetailsByBalance(5001.77));
+
+	}
 }
---
+++ src/main/java/com/wipro/bank/entity/Account.java
@@ -0,0 +1,40 @@
+package com.wipro.bank.entity;
+
+public class Account {
+	private int accountID;
+	public int getAccountID() {
+		return accountID;
+	}
+	public Account() {
+		super();
+	}
+	public Account(int accountID, String userName, double balance) {
+		super();
+		this.accountID = accountID;
+		this.userName = userName;
+		this.balance = balance;
+	}
+	@Override
+	public String toString() {
+		return "Account [accountID=" + accountID + ", userName=" + userName
+				+ ", balance=" + balance + "]";
+	}
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	private String userName;
+	private double balance;
+
+}
